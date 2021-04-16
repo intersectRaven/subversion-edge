e2773587b9dc5a0e503caf5cddd16606e42c7953
intersectRaven <rjgolo@gmail.com>
2021-04-16 12:37:24 +0800

    Always copy "dist" configuration files to data folder.

--- files/config/init/10_set-initial-data.sh
+++ files/config/init/10_set-initial-data.sh
@@ -3,3 +3,7 @@
 if [ ! -f "/opt/csvn/data/conf/csvn.conf" ]; then
     cp -r /opt/csvn/data-initial/* /opt/csvn/data
 fi
+
+# Always copy the "dist" configuration
+# files to the data folder.
+cp -f /opt/csvn/dist/* /opt/csvn/data/conf
